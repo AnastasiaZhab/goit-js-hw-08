fed6a9b5c9cccb2d880884cb1c05d91fff3472e8
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2021-11-17 23:04:58 +0200

    выполняет задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageGallery = document.querySelector('.gallery');
+const imageList = createGallery(galleryItems);
+
+
+function createGallery(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+        <li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    }
+    ).join('');
+};
+
+imageGallery.insertAdjacentHTML('afterbegin', imageList);
+
+
+
+let ligthBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsPosition: 'botton',
+    captionDelay: '250',
+
+});
